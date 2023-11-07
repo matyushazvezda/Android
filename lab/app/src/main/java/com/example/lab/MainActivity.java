7947c0fe00fbe895cf7f4443d8d21f4355c5b7c7
matyushazvezda <matyushazvezda@yandex.ru>
2023-11-07 22:56:04 +0800

    movies

--- lab/app/src/main/java/com/example/lab/MainActivity.java
+++ lab/app/src/main/java/com/example/lab/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -7,42 +8,89 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editText;
-    private EditText editText2;
-    private TextView textView;
-
+    private TextView textViewTittle, textViewYear, textViewDirector, textViewGenre, textViewRating;
+    private int k =0;
+    private List<Integer> selectedMovieIndices = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editTextText);
-        editText2 = findViewById(R.id.editTextText2);
-        textView = findViewById(R.id.textView);
+        textViewTittle = findViewById(R.id.textView);
+        textViewYear = findViewById(R.id.textView2);
+        textViewDirector = findViewById(R.id.textView3);
+        textViewGenre = findViewById(R.id.textView4);
+        textViewRating = findViewById(R.id.textView5);
+        //textView = findViewById(R.id.textView);
     }
 
+
     public void clickButton(View v) {
-        // Объявим числовые переменные
-        double a, b, c;
+        AssetManager assetManager = getAssets();
 
-        // Считаем с editText и editText2 текстовые значения
-        String s1 = editText.getText().toString();
-        String s2 = editText2.getText().toString();
+        try {
+            InputStream is = assetManager.open("movies.json");
+            String js = convertStreamToString(is);
 
-        // Преобразуем текстовые переменные в числовые значения
-        a = Double.parseDouble(s1);
-        b = Double.parseDouble(s2);
+            Gson g = new Gson();
+            ArrayList<Movie> movies = g.fromJson(js, new TypeToken<List<Movie>>() {}.getType());
 
-        // Проведем с числовыми переменными нужные действия
-        c = a + b;
+            if (selectedMovieIndices.size() < movies.size()) {
+                Random random = new Random();
 
-        // Преобразуем ответ в число
-        String s = Double.toString(c);
+                int moviesLeft = movies.size() - selectedMovieIndices.size();
+                int randomIndex = random.nextInt(moviesLeft);
 
-        // Выведем текст в textView
-        textView.setText(s);
+                for (int i = 0, j = 0; i < movies.size(); ++i) {
+                    if (!selectedMovieIndices.contains(i)) {
+                        if (j == randomIndex) {
+                            randomIndex = i;
+                            break;
+                        }
+                        ++j;
+                    }
+                }
+
+                selectedMovieIndices.add(randomIndex);
+
+                Movie movie = movies.get(randomIndex);
+                String movieTitle = movie.getTitle();
+                int movieYear = movie.getYear();
+                String movieDirector = movie.getDirector();
+                String movieGenre = movie.getGenre();
+                double movieRating = movie.getRating();
+
+                textViewTittle.setText(movieTitle);
+                textViewYear.setText("Год: "+String.valueOf(movieYear));
+                textViewDirector.setText("Режиссер: "+movieDirector);
+                textViewGenre.setText("Жанр: "+movieGenre);
+                textViewRating.setText("Рейтинг: "+String.valueOf(movieRating));
+            } else {
+                textViewTittle.setText("Все фильмы просмотрены");
+                textViewYear.setText("");
+                textViewDirector.setText("");
+                textViewGenre.setText("");
+                textViewRating.setText("");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private String convertStreamToString(InputStream is) {
+        java.util.Scanner scanner = new java.util.Scanner(is).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
     }
 }
---
+++ lab/app/src/main/java/com/example/lab/Movie.java
@@ -0,0 +1,30 @@
+package com.example.lab;
+
+public class Movie {
+    private String title;
+    private int year;
+    private String director;
+    private String genre;
+    private double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
+
