06bcb2ba1f54fb11a042bc28fe741be464a76d87
matyushazvezda <matyushazvezda@yandex.ru>
2023-11-10 13:22:46 +0800

    final tickets

--- Tickets/app/src/main/java/com/example/tickets/MainActivity.java
+++ Tickets/app/src/main/java/com/example/tickets/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tickets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.app.DatePickerDialog;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextDepartureDate = findViewById(R.id.editTextDepartureDate);
+        EditText editTextArrivalDate = findViewById(R.id.editTextArrivalDate);
+        editTextDepartureDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(editTextDepartureDate);
+            }
+        });
+
+        editTextArrivalDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(editTextArrivalDate);
+            }
+        });
+    }
+
+    private void showDatePickerDialog(final EditText editText) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // В этом методе обработайте выбранную дату и установите ее в поле ввода
+                        String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        editText.setText(selectedDate);
+                    }
+                },
+                year, month, dayOfMonth);
+
+        datePickerDialog.show();
     }
 }
